d6002bf53e3860094c7fb5b376c93117243bbed0
Илья Олейников <oleynikovis@yandex.ru>
2024-07-23 16:14:02 +0300

    add utils & constants

--- main1.py
+++ main1.py
@@ -3,25 +3,16 @@ import re
 import pandas as pd
 
 from constants import (COLUMN_TITLES_DZ, COLUMN_TITLES_TOP_10, END_PATTERN,
-                       FILES, INN_PATTERN, INPUT_DIR, OUTPUT_DIR, START_PATTERN)
+                       FILES, INN_PATTERN, INPUT_DIR, START_PATTERN)
+from utils import get_osv_number, save_result
 
 
-def get_osv_number(file):
-    if '60' in file:
-        return '60'
-    elif '62' in file:
-        return '62'
-    elif '76' in file:
-        return '76'
-    else:
+def get_top_10_counterparts(df, osv_number):
+
+    if osv_number == '76':
         return None
 
-
-def get_dataframe(file, osv_number):
-    return pd.read_excel(f'{INPUT_DIR}{file}', header=None)
-
-
-def get_top_10_counterparts(dataframe, osv_number):
+    df.drop(df.columns[[1, 2, 5, 6]], axis=1, inplace=True)
     filtered_rows = []
     capture = False
 
@@ -34,38 +25,24 @@ def get_top_10_counterparts(dataframe, osv_number):
         if re.match(START_PATTERN, first_cell_value):
             capture = True
 
-    return pd.DataFrame(filtered_rows).fillna(0)
-
-    df.drop(
-        [COLUMN_TITLES[1], COLUMN_TITLES[2], COLUMN_TITLES[5], COLUMN_TITLES[6]],
-        axis=1,
-        inplace=True
-    )
+    cleared_df = pd.DataFrame(filtered_rows).fillna(0)
 
     if osv_number == '60':
-        df.drop(
-            [COLUMN_TITLES[3]],
-            axis=1,
-            inplace=True
-        )
-        turnover_sum = df[COLUMN_TITLES[4]].sum()
-        df_top_10 = df.sort_values(by=COLUMN_TITLES[4], ascending=False).head(10)
-        df_top_10['Доля'] = df_top_10[COLUMN_TITLES[4]] / turnover_sum
-        return df_top_10
+        cleared_df.drop(cleared_df.columns[1], axis=1, inplace=True)
+    else:
+        cleared_df.drop(cleared_df.columns[2], axis=1, inplace=True)
+    cleared_df.to_excel('result.xlsx', index=False)
 
-    df.drop(
-        [COLUMN_TITLES[4]],
-        axis=1,
-        inplace=True
-    )
-    turnover_sum = df[COLUMN_TITLES[3]].sum()
-    df_top_10 = df.sort_values(by=COLUMN_TITLES[3], ascending=False).head(10)
-    df_top_10['Доля'] = df_top_10[COLUMN_TITLES[3]] / turnover_sum
+    turnover_sum = cleared_df[cleared_df.columns[1]].sum()
+    df_top_10 = cleared_df.sort_values(by=cleared_df.columns[1], ascending=False).head(10)
+    df_top_10['any_name'] = df_top_10[cleared_df.columns[1]] / turnover_sum * 100
+    df_top_10.columns = COLUMN_TITLES_TOP_10
+
     return df_top_10
 
 
 def get_overdue_receivable(dataframe):
-    pass
+    return None
 
 
 if __name__ == '__main__':
@@ -73,22 +50,21 @@ if __name__ == '__main__':
 
         # 1 проверяем номер ОСВ в названии файла
         osv_number = get_osv_number(file)
-        if osv_number is None:
+        if not osv_number:
             continue
 
         # 2 получаем датафрейм
-        df = get_dataframe(file, osv_number)
+        dataframe = pd.read_excel(f'{INPUT_DIR}{file}', header=None)
 
         # 3 обрабатываем данные в зависимости от номера осв
+
         # 3.1 отдельная функция для осв 60 и 62 по крупным контрагентам
-        top_10_counterparts = get_top_10_counterparts(df, osv_number)
-
+        top_10_counterparts = get_top_10_counterparts(dataframe, osv_number)
         # 3.2 отдельная для 60/62/76 по просроченной ДЗ
-        overdue_receivable = get_overdue_receivable(df)
+        overdue_receivable = get_overdue_receivable(dataframe)
+
         # 4 наводим красоту и сохраняем результат
-
-        if file.endswith('xls'):  # pandas не сохраняет старый формат
-            file = file.replace('xls', 'xlsx')
-        top_10_counterparts.to_excel(f'{OUTPUT_DIR}{file}', index=False)
-        overdue_receivable.to_excel(f'{OUTPUT_DIR}{file}', index=False)
-
+        if top_10_counterparts:
+            save_result(top_10_counterparts, file)
+        if overdue_receivable:
+            save_result(overdue_receivable, file)
---
+++ utils.py
@@ -0,0 +1,18 @@
+from constants import OUTPUT_DIR
+
+
+def get_osv_number(file):
+    if '60' in file:
+        return '60'
+    elif '62' in file:
+        return '62'
+    elif '76' in file:
+        return '76'
+    else:
+        return None
+
+
+def save_result(file, df):
+    if file.endswith('xls'):  # pandas не сохраняет старый формат
+        file = file.replace('xls', 'xlsx')
+    df.to_excel(f'{OUTPUT_DIR}{file}', index=False)
