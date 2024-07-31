9a7c45a7ffbc010122ccdc91723867c0108cb768
Илья Олейников <oleynikovis@yandex.ru>
2024-07-31 10:25:14 +0300

    first success

--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import tabula
 
-from constants import OUTPUT_DIR
+from constants import OUTPUT_DIR, INPUT_DIR
 
 
-def get_osv_number(file):
+def get_osv_number(file: str):
     if '60' in file:
         return '60'
     elif '62' in file:
         return '62'
     elif '76' in file:
         return '76'
+    elif file.endswith('.pdf'):
+        parce_pdf(file)
     else:
         return None
 
@@ -17,3 +20,30 @@ def get_osv_number(file):
 def save_result(df, osv_number=None, name=None):
     path = f'{OUTPUT_DIR}{name}_{osv_number}.xlsx'
     df.to_excel(path, index=False)
+
+
+def parce_pdf(file):
+    tables = tabula.read_pdf(
+        f'{INPUT_DIR}{file}',
+        pages='all',
+        multiple_tables=True)
+
+    if tables:
+        # Создаем пустой Excel-файл
+        writer = pd.ExcelWriter('all_tables.xlsx')
+
+        # Проходимся по каждой найденной таблице
+        for i, table in enumerate(tables):
+            # Преобразуем таблицу в DataFrame (если это ещё не DataFrame)
+            if not isinstance(table, pd.DataFrame):
+                table = pd.DataFrame(table)
+
+            # Сохраняем таблицу в отдельном листе Excel
+            table.to_excel(writer, sheet_name=f'Sheet_{i + 1}', index=False)
+
+        # Сохраняем Excel-файл
+        writer._save()
+
+        print(f"Все таблицы из файла '{file}' сохранены в файл 'all_tables.xlsx'")
+    else:
+        print("В PDF-файле не найдено таблиц.")
