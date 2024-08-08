eaae0675c4854b2fa4b0094fdcaa19b8e9497a23
Илья Олейников <oleynikovis@yandex.ru>
2024-08-08 16:24:43 +0300

    all_results_in_one_file

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 import os
 
-from file_processing import process_60_62_76, process_pdf, process_60_62, process_50_51_52
 
 COLUMN_TITLES_TOP_10 = [
     'Контрагенты',
@@ -27,10 +26,3 @@ FILES = os.listdir(INPUT_DIR)
 PERCENT = 100
 K = 1000
 KK = 1000000
-
-FILE_HANDLERS = {
-    process_60_62: ('60', '62'),
-    process_60_62_76: ('60', '62', '76'),
-    process_50_51_52: ('50', '51', '52'),
-    process_pdf: ('.pdf',)
-}
--- utils.py
+++ utils.py
@@ -4,42 +4,24 @@ import tabula
 from constants import OUTPUT_DIR, INPUT_DIR
 
 
-def get_osv_number(file: str):
-    if '60' in file:
-        return '60'
-    elif '62' in file:
-        return '62'
-    elif '76' in file:
-        return '76'
-    elif file.endswith('.pdf'):
-        parce_pdf(file)
-    else:
-        return None
-
-
 def save_result(df, osv_number=None, name=None):
     path = f'{OUTPUT_DIR}{name}_{osv_number}.xlsx'
     df.to_excel(path, index=False)
 
 
-def parce_pdf(file):
-    tables = tabula.read_pdf(
-        f'{INPUT_DIR}{file}',
-        pages='all',
-        multiple_tables=True)
-
-    if tables:
-        # Создаем пустой Excel-файл
-        writer = pd.ExcelWriter('all_tables.xlsx')
-
-        # Проходимся по каждой найденной таблице
+def get_dataframe(file, file_suffix):
+    if file_suffix == 'pdf':
+        tables = tabula.read_pdf(
+            f'{INPUT_DIR}{file}',
+            pages='all',
+            multiple_tables=True
+        )
         for i, table in enumerate(tables):
-            # Преобразуем таблицу в DataFrame (если это ещё не DataFrame)
             if not isinstance(table, pd.DataFrame):
                 table = pd.DataFrame(table)
-
-            # Сохраняем таблицу в отдельном листе Excel
-            table.to_excel(writer, sheet_name=f'Sheet_{i + 1}', index=False)
-
-        # Сохраняем Excel-файл
-        writer._save()
+            return table
+    excel = pd.read_excel(
+        f'{INPUT_DIR}{file}',
+        header=None,
+    )
+    return excel
